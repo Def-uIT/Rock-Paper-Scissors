dae28a3cf0b76830a092b3731279a743bc6ca313
Def-ulT <vadgunbin@yandex.ru>
2023-10-15 16:23:35 +0300

    make site layout

--- js.js
+++ js.js
@@ -1,50 +1,26 @@
-getComputerChoice = function() {
-    const array = ['rock', 'paper', 'scissors'];
-    return array[Math.floor(Math.random() * array.length)];
+const compChoice = function() {
+    const AllItems = ["Rock", "Paper", "Sciccors"];
+    let choice = Math.random * AllItems.length;
+    return choice;
 }
 
-getPlayerChoice = function() {
-    return prompt('Make your choice!').toLowerCase();
-}
+//const playerChoice = on click;
 
-while (true) {
-    checkWin = function(computerSelection, playerSelection) {
-        if (computerSelection == 'rock' && playerSelection == 'paper') {
-            answer = `You win! ${playerSelection} beats ${computerSelection}`;
-            return answer;
-        } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
-            answer = `You lost! ${computerSelection} beats ${playerSelection}`;
-            return answer;
-        } else if (computerSelection == 'rock' && playerSelection == 'rock') {
-            answer = `Tie! ${computerSelection} and ${playerSelection} are equal.`;
-            return answer;
+const playRound = function(playerChoice, compChoice) {
+    const winArray = [ [0, 2, 1],
+                        [1, 0, 2],
+                        [2, 1, 0]  ];
     
-        } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
-            answer = `You win! ${playerSelection} beats ${computerSelection}`;
-            return answer;
-        } else if (computerSelection == 'paper' && playerSelection == 'rock') {
-            answer = `You lost! ${computerSelection} beats ${playerSelection}`;
-            return answer;
-        } else if (computerSelection == 'paper' && playerSelection == 'paper') {
-            answer = `Tie! ${computerSelection} and ${playerSelection} are equal.`;
-            return answer;
-    
-        } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
-            answer = `You win! ${playerSelection} beats ${computerSelection}`;
-            return answer;
-        } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
-            answer = `You lost! ${computerSelection} beats ${playerSelection}`;
-            return answer;
-        } else if (computerSelection == 'scissors' && playerSelection == 'scissors') {
-            answer = `Tie! ${computerSelection} and ${playerSelection} are equal.`;
-            return answer;
-        }
+    const result = winArray[playerChoice, compChoice];
+
+    if (result == 0) {
+        output.textContent = "Tie! You chose ${playerChoice} and Computer chose ${compChoice}!";
+    } else if (result == 1){
+        output.textContent = "You win! You chose ${playerChoice} and Computer chose ${compChoice}!";
+        
+    } else {
+        output.textContent = "You lost! You chose ${playerChoice} and Computer chose ${compChoice}!";
     }
-    
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    console.log(computerSelection);
-    console.log(playerSelection);
-    
-    console.log(checkWin(computerSelection, playerSelection));
 }
+
+
